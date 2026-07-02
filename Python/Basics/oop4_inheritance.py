19537b90982bb21a53b88ce2e5f7b4a0dc69f834
bhashyavi22 <bhashyavi22@gmail.com>
2026-07-02 20:30:18 +0530

    Add Python inheritance and super examples

--- Python/Basics/oop4_inheritance.py
+++ Python/Basics/oop4_inheritance.py
@@ -1,4 +1,4 @@
-#program 1
+#program 1-Basic Inhertance
 class Animal:
     def sound(self):
         print("Animals make sounds")
@@ -7,7 +7,7 @@ class Dog(Animal):
 dog=Dog()
 dog.sound()
 print("---------------------------")
-#program 2
+#program 2-Vehicle Inheritance
 class Vehicle:
     def start(self):
         print("Vehicle started")
@@ -16,7 +16,7 @@ class Car(Vehicle):
 car=Car()
 car.start()
 print("---------------------------")
-#program 3
+#program 3-Child Class Own Method
 class Animal:
     def eat(self):
         print("Animal is eating")
@@ -27,7 +27,7 @@ dog=Dog()
 dog.eat()
 dog.bark()
 print("---------------------------")
-#program 4
+#program 4-Method Overriding
 class Animal:
     def sound(self):
         print("Animals make sounds")
@@ -37,3 +37,25 @@ class Dog(Animal):
 
 dog=Dog()
 dog.sound()
+print("---------------------------")
+# Program 5 - Using super() with constructors
+class Animal:
+    def __init__(self):
+        print("Animal constructor")
+class Dog(Animal):
+    def __init__(self):
+        super().__init__()
+        print("Dog constructor")
+
+dog=Dog()
+print("--------------------------")
+# Program 6 - Practice using super()
+class Person:
+    def __init__(self):
+        print("Person constructor")
+class Student(Person):
+    def __init__(self):
+        super().__init__()
+        print("Student constructor")
+
+student=Student()
