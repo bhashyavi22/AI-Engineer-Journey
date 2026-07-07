ffec18f1cdfea0b4822ee422b97b9e66c1fdb0c2
bhashyavi22 <bhashyavi22@gmail.com>
2026-07-07 22:37:08 +0530

    Add polymorphism examples in Python OOP

--- Python/Basics/polymorphism_basic.py
+++ Python/Basics/polymorphism_basic.py
@@ -1,3 +1,4 @@
+#program 1-Basic Polymorphism
 class Dog:
     def sound(self):
         print("Dog barks")
@@ -11,3 +12,20 @@ cat = Cat()
 
 dog.sound()
 cat.sound()
+print("\n---------------------------\n")
+
+#program 2-Polymorphism using a Common Function
+class Dog:
+    def sound(self):
+        print("Dog barks")
+class Cat:
+    def sound(self):
+        print("Cat meows")
+def animal_sound(animal):
+    animal.sound()
+
+dog=Dog()
+cat=Cat()
+
+animal_sound(dog)
+animal_sound(cat)
