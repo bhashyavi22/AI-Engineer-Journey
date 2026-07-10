6925d55bbbc9c48b662517c1f45d0272184ca99a
bhashyavi22 <bhashyavi22@gmail.com>
2026-07-10 22:39:45 +0530

    Add encapslation example with Protected Member

--- Python/Basics/encapsulation_basics.py
+++ Python/Basics/encapsulation_basics.py
@@ -5,3 +5,10 @@ class Student:
 student=Student()
 print(student.name)
 print("---------------------------")
+# program 2 - Protected Member
+class Student:
+    def __init__(self):
+        self._name="Bhashyavi"
+student=Student()
+print(student._name)
+print("---------------------------")
