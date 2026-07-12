b1ed06b4c2101b325901eb64ed93fd0f0d1007f1
bhashyavi22 <bhashyavi22@gmail.com>
2026-07-12 16:39:51 +0530

    Add NumPy attributes

--- Python/NumPy/numpy_attributes.py
+++ Python/NumPy/numpy_attributes.py
@@ -1,7 +1,26 @@
-# program 1-shape
+# program 1-shape(Size of each dimension)
 import numpy as np
 arr=np.array([[1,2,3],
               [4,5,6]])
 print(arr.shape)
 arr1=np.array([10,20,30,40])
 print(arr1.shape)
+print("------------------------------")
+
+#program 2 - ndim(Number of dimensions)
+import numpy as np
+arr=np.array([[1,2,3],
+              [4,5,6]])
+print(arr.ndim)
+print("-------------------------------")
+
+# program 3 - size(total no.of elements)
+import numpy as np
+arr=np.array([[1,2,3],[4,5,6]])
+print(arr.size)
+print("-------------------------------")
+
+# program 4 - dtype(data type of elements)
+import numpy as np
+arr=np.array([10,20,30])
+print(arr.dtype)
