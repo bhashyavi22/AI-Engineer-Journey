eb3de974b3625134654b7a950c061d7fa290da33
bhashyavi22 <bhashyavi22@gmail.com>
2026-07-16 21:47:21 +0530

    Add NumPy aggregation function examples

---
+++ Python/NumPy/aggregation_functions.py
@@ -0,0 +1,46 @@
+# Program 1 - sum()
+import numpy as np
+arr=np.array([10,20,30,40])
+print(arr.sum())
+print("-----------------------------")
+
+# Program 2 - mean()
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print(arr.mean())
+print("------------------------------")
+
+# Program 3 - min()
+import numpy as np
+arr=np.array([10,25,5,40,15])
+print(arr.min())
+print("------------------------------")
+
+# Program 4 - max()
+import numpy as np
+arr=np.array([10,25,5,40,15])
+print(arr.max())
+print("------------------------------")
+
+# Program 5 - argmin() index of smallest element
+import numpy as np
+arr=np.array([50,20,70,10,30])
+print(arr.argmin())
+print("------------------------------")
+
+# Program 6 - argmax() index of largest element
+import numpy as np
+arr=np.array([10,25,5,40,15])
+print(arr.argmax())
+print("-------------------------------")
+
+# Program 7 - std() Standard Deviation
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print(arr.std())
+print("-------------------------------")
+
+# Program 8 - var() Variance
+import numpy as np
+arr=np.array([10,20,30,40,50])
+print(arr.var())
