3023ac626d8966ab6915236d197e058a2d48ca3a
bhashyavi22 <bhashyavi22@gmail.com>
2026-09-13 23:27:50 +0530

    solved prime number

---
+++ Python/Basics/prime_num.py
@@ -0,0 +1,9 @@
+def prime(num):
+    if num<=1:
+        return "not a prime"
+    for i in range(2,num):
+        if num%i==0:
+            return "not a prime"
+    return "prime"
+num=int(input("Enter="))
+print(prime(num))
